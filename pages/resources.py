93720c0264731edc67d577afae75541780b75d17
lueng0818 <34761291+lueng0818@users.noreply.github.com>
2025-07-17 21:38:35 +0800

    Update resources.py

--- pages/resources.py
+++ pages/resources.py
@@ -11,3 +11,16 @@ def show():
                 st.download_button("點此下載 PDF", f, file_name='guide.pdf')
         else:
             st.error("訂閱失敗，請稍後再試。")
+
+    st.markdown("---")
+    st.markdown("### 延伸免費資源")
+    st.markdown(
+        """
+        <ul class='list-disc list-inside'>
+          <li><a href='https://maya-self-explore.streamlit.app/' target='_blank'>自我探索</a></li>
+          <li><a href='https://maya-wealth.streamlit.app/' target='_blank'>金錢能量頻率</a></li>
+          <li><a href='https://maya-emotion.streamlit.app/' target='_blank'>情感關係解讀</a></li>
+        </ul>
+        """,
+        unsafe_allow_html=True
+    )
